5dc4bf8ed0c4cacc4b9763bba9231aaff6418c14
Shahala <sahalashl123@gmail.com>
2025-03-01 00:26:53 +0300

    Add HTML template and update index.js for root element rendering

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const rootElement = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- 
-    <App />
-  
-);
-
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
 
